6049f6a29a8b8aa42d73e1c2ff090af52294208a
evan-fitzpatrick <evan.fitzpatrick@gmail.com>
2021-04-22 13:33:45 -0600

    emit an event when the position of the board is changed

--- contracts/gamemove.sol
+++ contracts/gamemove.sol
@@ -4,13 +4,16 @@ import "./gamehelper.sol";
 
 contract GameMove is GameHelper {
 
+    event moveSuccessful(uint gameId, string gamePosition);
+
     function move(uint _gameId) external onlyPlayerOf(_gameId) {
         //leaving this blank until FEN parsing is implemented
 
     }
 
-    function setPosition(uint _gameId, string calldata position) external onlyPlayerOf(_gameId){
+    function setPosition(uint _gameId, string calldata _position) external onlyPlayerOf(_gameId){
         //No validation to make sure that the position is valid, or follows from a legal move of previous position, yet
-        games[_gameId].position = position;
+        games[_gameId].position = _position;
+        emit moveSuccessful(_gameId, _position);
     }
 }
